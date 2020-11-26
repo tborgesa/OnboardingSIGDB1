1fbf13b7960b93eb5a005329d32a26503dc26b03
Thiago Borges Amaral <thiago.amaral@db1.com.br>
2020-11-26 11:53:20 -0300

    Inicio projeto (.csproj) vazio

---
+++ src/OnboardingSIGDB1.Api/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnboardingSIGDB1.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PIngController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("pong");
+        }
+    }
+}
---
+++ src/OnboardingSIGDB1.Api/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace OnboardingSIGDB1.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
