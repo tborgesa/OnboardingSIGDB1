0b19b3ca063cba4da3fb12ac3d9b287c6ee9b726
Thiago Borges Amaral <thiago.amaral@db1.com.br>
2020-11-30 15:35:00 -0300

    Swagger Funcionando

--- src/OnboardingSIGDB1.Api/Startup.cs
+++ src/OnboardingSIGDB1.Api/Startup.cs
@@ -3,6 +3,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace OnboardingSIGDB1.Api
 {
@@ -15,25 +25,48 @@ namespace OnboardingSIGDB1.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+     public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "OnboardingSIGDB1",
+                        Version = "v1",
+                        Description = "Projeto de OnBoarding para o projeto do SENAC - DB1",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Thiago Borges Amaral",
+                            Url = new Uri("https://github.com/tborgesa")
+                        }
+                    });
+
+                string caminhoAplicacao =
+                    PlatformServices.Default.Application.ApplicationBasePath;
+                string nomeAplicacao =
+                    PlatformServices.Default.Application.ApplicationName;
+                string caminhoXmlDoc =
+                    Path.Combine(caminhoAplicacao, $"{nomeAplicacao}.xml");
+
+                c.IncludeXmlComments(caminhoXmlDoc);
+            });
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("v1/swagger.json", $"API V1");
+            });
+
             app.UseMvc();
         }
     }
