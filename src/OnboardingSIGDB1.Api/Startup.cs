defbf999a3bc7c879447dbd68f042872a7cec60f
Thiago Borges Amaral <thiago.amaral@db1.com.br>
2020-11-30 17:24:36 -0300

    Criação da SwaggerMiddlewareExtension

--- src/OnboardingSIGDB1.Api/Startup.cs
+++ src/OnboardingSIGDB1.Api/Startup.cs
@@ -3,16 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.Extensions.PlatformAbstractions;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Swagger;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using OnboardingSIGDB1.Api._Base.Middleware;
 
 namespace OnboardingSIGDB1.Api
 {
@@ -25,33 +16,11 @@ namespace OnboardingSIGDB1.Api
 
         public IConfiguration Configuration { get; }
 
-     public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1",
-                    new OpenApiInfo
-                    {
-                        Title = "OnboardingSIGDB1",
-                        Version = "v1",
-                        Description = "Projeto de OnBoarding para o projeto do SENAC - DB1",
-                        Contact = new OpenApiContact
-                        {
-                            Name = "Thiago Borges Amaral",
-                            Url = new Uri("https://github.com/tborgesa")
-                        }
-                    });
-
-                //string caminhoAplicacao =
-                //    PlatformServices.Default.Application.ApplicationBasePath;
-                //string nomeAplicacao =
-                //    PlatformServices.Default.Application.ApplicationName;
-                //string caminhoXmlDoc =
-                //    Path.Combine(caminhoAplicacao, $"{nomeAplicacao}.xml");
-
-                //c.IncludeXmlComments(caminhoXmlDoc);
-            });
+            services.AddSwaggerGen();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -61,10 +30,7 @@ namespace OnboardingSIGDB1.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("v1/swagger.json", $"API V1");
-            });
+            app.UseSwaggerDoc(env);
 
             app.UseMvc();
         }
---
+++ src/OnboardingSIGDB1.Api/_Base/Middleware/SwaggerMiddlewareExtension.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace OnboardingSIGDB1.Api._Base.Middleware
+{
+    public static class SwaggerMiddlewareExtension
+    {
+        public static void AddSwaggerDoc(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "OnboardingSIGDB1",
+                        Version = "v1",
+                        Description = "Projeto de OnBoarding para o projeto do SENAC - DB1",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Thiago Borges Amaral",
+                            Url = new Uri("https://github.com/tborgesa")
+                        }
+                    });
+            });
+        }
+
+        public static void UseSwaggerDoc(this IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", $"OnboardingSIGDB1 V1 ({env.EnvironmentName})");
+            });
+        }
+    }
+}
