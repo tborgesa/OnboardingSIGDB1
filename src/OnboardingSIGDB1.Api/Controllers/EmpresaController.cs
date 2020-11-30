7daed5d0d834631b868ee12be5ffc8a5250247bd
Thiago Borges Amaral <thiago.amaral@db1.com.br>
2020-11-30 20:01:45 -0300

    EmpresaController - Parcial - Sem funcionar

---
+++ src/OnboardingSIGDB1.Api/Controllers/EmpresaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnboardingSIGDB1.Api._Base.Controllers;
+using OnboardingSIGDB1.Domain.Empresas.Dto;
+using OnboardingSIGDB1.Domain.Empresas.Interfaces;
+using System.Threading.Tasks;
+
+namespace OnboardingSIGDB1.Api.Controllers
+{
+    public class EmpresaController : OnboardingSIGDB1Controller
+    {
+        private readonly IArmazenadorDeEmpresa _armazenadorDeEmpresa;
+
+        public EmpresaController(IArmazenadorDeEmpresa armazenadorDeEmpresa)
+        {
+            _armazenadorDeEmpresa = armazenadorDeEmpresa;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.CompletedTask;
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            await Task.CompletedTask;
+            return Ok();
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Post(EmpresaDto empresaDto)
+        {
+            await _armazenadorDeEmpresa.ArmazenarAsync(empresaDto);
+
+            await Task.CompletedTask;
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, EmpresaDto empresaDto)
+        {
+            await Task.CompletedTask;
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await Task.CompletedTask;
+            return Ok();
+        }
+    }
+}
