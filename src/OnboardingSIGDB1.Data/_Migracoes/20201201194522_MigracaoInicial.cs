f20f5dd289e5361403fad4deb3a2ff511a19aa97
Thiago Borges Amaral <thiago.amaral@db1.com.br>
2020-12-01 17:11:55 -0300

    Migration
    - Criando a primeira migracao com banco vazio

---
+++ src/OnboardingSIGDB1.Data/_Migracoes/20201201194522_MigracaoInicial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnboardingSIGDB1.Data._Migracoes
+{
+    public partial class MigracaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
