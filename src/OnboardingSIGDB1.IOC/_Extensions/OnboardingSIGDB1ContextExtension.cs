bddd26d4e90006be20dbdc7890760bfe79386b22
Thiago Borges Amaral <thiago.amaral@db1.com.br>
2020-12-02 15:36:33 -0300

    Anonimizando os parametros do console em Release Configuration

--- src/OnboardingSIGDB1.IOC/_Extensions/OnboardingSIGDB1ContextExtension.cs
+++ src/OnboardingSIGDB1.IOC/_Extensions/OnboardingSIGDB1ContextExtension.cs
@@ -1,18 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using OnboardingSIGDB1.Data._Contexto;
 using OnboardingSIGDB1.Domain._Base.Entidades.AppSettings;
+using OnboardingSIGDB1.Domain._Base.Resources;
+using System;
 
 namespace OnboardingSIGDB1.IOC._Extensions
 {
     public static class OnboardingSIGDB1ContextExtension
     {
+#if DEBUG
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+#endif
+
         public static void AddOnboardingSIGDB1Context(this IServiceCollection services,
             AppSettingsDoOnboardingSIGDB1 appSettingsDoOnboardingSIGDB1)
         {
             services.AddDbContext<OnboardingSIGDB1Context>(options =>
              {
                  options.UseSqlServer(appSettingsDoOnboardingSIGDB1.StringDeConexoes.OnboardingSIGDB1);
+
+#if DEBUG
+                 options.
+                     UseLoggerFactory(_logger).
+                     EnableSensitiveDataLogging();
+#endif
              });
         }
     }
